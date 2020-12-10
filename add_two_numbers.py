73c44ae45b62bd0839359e25c83447ba608b1fd0
Govind <govindajithkumar@gmail.com>
2020-12-09 19:37:22 -0500

    Two sums linkedlist problem completed!

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -1,10 +1,8 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
         num1 = ''
@@ -17,9 +15,17 @@ class Solution:
             num2 += str(l2.val)
             l2 = l2.next
         num2 = int(''.join(reversed(num2)))
+
+        num_sum = num1 + num2
+
         num_sum = str(num1 + num2)
         num_sum = ''.join(reversed(num_sum))
         num_sum = list(num_sum)
-        for x in range(len(num_sum)):
-            num_sum[x] = int(num_sum[x])
-        print(list(num_sum))
+
+        l_ans_main = ListNode(num_sum[0])
+        l_ans = l_ans_main
+        for idx in range(1, len(num_sum)):
+            l_ans.next = ListNode(num_sum[idx])
+            l_ans = l_ans.next
+
+        return l_ans_main
