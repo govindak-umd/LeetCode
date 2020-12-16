3c4884b68490a27e1c03f79827d7ee1ec301a41e
Govind <govindajithkumar@gmail.com>
2020-12-16 09:05:52 -0500

    Initial Sudoku solver stub

---
+++ sudoku_solver.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findinRow(num):
+        pass
+
+    def findinColumn(num):
+        pass
+
+    def findinSquare(num):
+        for i in range(3):
+            for j in range(3):
+                pass
+
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
