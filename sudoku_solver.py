b8ea3768e54ae32f5c5a200ea138ed442cdffb59
Govind <govindajithkumar@gmail.com>
2020-12-16 16:42:17 -0500

    Added basic boolean funcs before testing

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -31,7 +31,7 @@ class Solution:
                     box_8.append(board[row][3:6])
                     box_9.append(board[row][6:9])
 
-            # print(box_1)
+            print(box_1)
             # print(box_2)
             # print(box_3)
             # print(box_4)
@@ -83,22 +83,24 @@ class Solution:
                     if pos not in box_9[0] and pos not in box_9[1] and pos not in box_9[
                         2] and pos not in all_possible_box_9:
                         all_possible_box_9.append(pos)
+            print(all_possible_box_1)
+            print(all_possible_box_2)
+            print(all_possible_box_3)
+            print(all_possible_box_4)
+            print(all_possible_box_5)
+            print(all_possible_box_6)
+            print(all_possible_box_7)
+            print(all_possible_box_8)
+            print(all_possible_box_9)
 
-        #             print(all_possible_box_1)
-        #             print(all_possible_box_2)
-        #             print(all_possible_box_3)
-        #             print(all_possible_box_4)
-        #             print(all_possible_box_5)
-        #             print(all_possible_box_6)
-        #             print(all_possible_box_7)
-        #             print(all_possible_box_8)
-        #             print(all_possible_box_9)
+        def findinRow(num, row):
+            if num in row:
+                return False
 
-        def findinRow(num):
-            pass
-
-        def findinColumn(num):
-            pass
+        def findinColumn(num, board, col):
+            for idx in range(9):
+                if num in board[idx][col]:
+                    return False
 
         def findinSquare(num):
             for i in range(3):
