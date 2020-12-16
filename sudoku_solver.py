baa7cee28a126a9ad79dc7ddc5fb166266b23df5
Govind <govindajithkumar@gmail.com>
2020-12-16 09:50:25 -0500

    Determined all possibilities for each square

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -5,17 +5,17 @@ class Solution:
         Do not return anything, modify board in-place instead.
 
         """
+        box_1 = []
+        box_2 = []
+        box_3 = []
+        box_4 = []
+        box_5 = []
+        box_6 = []
+        box_7 = []
+        box_8 = []
+        box_9 = []
 
         def extractEachSquare(board):
-            box_1 = []
-            box_2 = []
-            box_3 = []
-            box_4 = []
-            box_5 = []
-            box_6 = []
-            box_7 = []
-            box_8 = []
-            box_9 = []
 
             for row in range(9):
                 if 0 <= row <= 2:
@@ -31,26 +31,80 @@ class Solution:
                     box_8.append(board[row][3:6])
                     box_9.append(board[row][6:9])
 
-            print(box_1)
-            print(box_2)
-            print(box_3)
-            print(box_4)
-            print(box_5)
-            print(box_6)
-            print(box_7)
-            print(box_8)
-            print(box_9)
+            # print(box_1)
+            # print(box_2)
+            # print(box_3)
+            # print(box_4)
+            # print(box_5)
+            # print(box_6)
+            # print(box_7)
+            # print(box_8)
+            # print(box_9)
+
+        def checkPossibilities():
+            all_numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+            all_possible_box_1 = []
+            all_possible_box_2 = []
+            all_possible_box_3 = []
+            all_possible_box_4 = []
+            all_possible_box_5 = []
+            all_possible_box_6 = []
+            all_possible_box_7 = []
+            all_possible_box_8 = []
+            all_possible_box_9 = []
+
+            for pos in all_numbers:
+                for i in range(3):
+                    if pos not in box_1[0] and pos not in box_1[1] and pos not in box_1[
+                        2] and pos not in all_possible_box_1:
+                        all_possible_box_1.append(pos)
+                    if pos not in box_2[0] and pos not in box_2[1] and pos not in box_2[
+                        2] and pos not in all_possible_box_2:
+                        all_possible_box_2.append(pos)
+                    if pos not in box_3[0] and pos not in box_3[1] and pos not in box_3[
+                        2] and pos not in all_possible_box_3:
+                        all_possible_box_3.append(pos)
+                    if pos not in box_4[0] and pos not in box_4[1] and pos not in box_4[
+                        2] and pos not in all_possible_box_4:
+                        all_possible_box_4.append(pos)
+                    if pos not in box_5[0] and pos not in box_5[1] and pos not in box_5[
+                        2] and pos not in all_possible_box_5:
+                        all_possible_box_5.append(pos)
+                    if pos not in box_6[0] and pos not in box_6[1] and pos not in box_6[
+                        2] and pos not in all_possible_box_6:
+                        all_possible_box_6.append(pos)
+                    if pos not in box_7[0] and pos not in box_7[1] and pos not in box_7[
+                        2] and pos not in all_possible_box_7:
+                        all_possible_box_7.append(pos)
+                    if pos not in box_8[0] and pos not in box_8[1] and pos not in box_8[
+                        2] and pos not in all_possible_box_8:
+                        all_possible_box_8.append(pos)
+                    if pos not in box_9[0] and pos not in box_9[1] and pos not in box_9[
+                        2] and pos not in all_possible_box_9:
+                        all_possible_box_9.append(pos)
+
+        #             print(all_possible_box_1)
+        #             print(all_possible_box_2)
+        #             print(all_possible_box_3)
+        #             print(all_possible_box_4)
+        #             print(all_possible_box_5)
+        #             print(all_possible_box_6)
+        #             print(all_possible_box_7)
+        #             print(all_possible_box_8)
+        #             print(all_possible_box_9)
+
+        def findinRow(num):
+            pass
+
+        def findinColumn(num):
+            pass
+
+        def findinSquare(num):
+            for i in range(3):
+                for j in range(3):
+                    pass
 
         extractEachSquare(board)
-
-    def findinRow(num):
-        pass
-
-    def findinColumn(num):
-        pass
-
-    def findinSquare(num):
-        for i in range(3):
-            for j in range(3):
-                pass
+        checkPossibilities()
 
